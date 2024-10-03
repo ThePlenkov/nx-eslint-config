eb7e04d1dec8f4494cccd5447ac5de1afb1f6326
ThePlenkov <petr.plenkov@gmail.com>
2024-10-03 14:45:37 +0200

    build in CI

--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,4 @@
 import typescript from '@nx/eslint-plugin/typescript.js'
+import { configs } from 'typescript-eslint';
 
-export default [...typescript.configs.typescript];
+export default [...typescript.configs.javascript,...typescript.configs.typescript];
